f64c4506f26ee85aaddf6e600c9ba2cf336a3a12
Spencer <sk389@byu.edu>
2023-01-23 09:32:13 -0700

    Created printBoard, printWinner, and format for printing the board

--- TicTacToe/display.cs
+++ TicTacToe/display.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class display
+    class Display
     {
+        public void printBoard(int[] board)
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + board[0] + "  |  " + board[1] + "  |  " + board[2] + "  ");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + board[3] + "  |  " + board[4] + "  |  " + board[5] + "  ");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + board[6] + "  |  " + board[7] + "  |  " + board[8] + "  ");
+            Console.WriteLine("     |     |     ");
+        }
+        public void printWinner(int[] board)
+        {
+
+        }
     }
 }
