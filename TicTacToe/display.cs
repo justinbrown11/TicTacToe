cf63e85d57c9f6066fd1dbd41eee1229178b337e
Spencer <sk389@byu.edu>
2023-01-23 20:41:06 -0700

    Changed everything, "final" code

--- TicTacToe/display.cs
+++ TicTacToe/display.cs
@@ -8,7 +8,7 @@ namespace TicTacToe
 {
     class Display
     {
-        public void printBoard(int[] board)
+        public void printBoard(string[] board)
         {
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  " + board[0] + "  |  " + board[1] + "  |  " + board[2] + "  ");
@@ -19,10 +19,143 @@ namespace TicTacToe
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  " + board[6] + "  |  " + board[7] + "  |  " + board[8] + "  ");
             Console.WriteLine("     |     |     ");
+            printWinner(board);
         }
-        public void printWinner(int[] board)
+        public void printWinner(string[] board)
         {
+            string placeHolder = "";
+            for (int i = 0; i < 9; i++)
+            {
+                placeHolder += board[i];
+            }
 
+
+            //Horizontal
+            if ((board[0] == board[1]) && (board[1] == board[2]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            else if ((board[3] == board[4]) && (board[4] == board[5]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            else if ((board[6] == board[7]) && (board[7] == board[8]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            //Vertical
+            else if ((board[0] == board[3]) && (board[3] == board[6]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            else if ((board[1] == board[4]) && (board[4] == board[7]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            else if ((board[2] == board[5]) && (board[5] == board[8]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            //Diagonal
+            else if ((board[0] == board[4]) && (board[4] == board[8]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine("O");
+                }
+
+            }
+            else if ((board[2] == board[4]) && (board[4] == board[6]))
+            {
+                if (board[0] == "X")
+                {
+                    Console.WriteLine("X"); //"X"
+                }
+                else
+                {
+                    Console.WriteLine("O"); //"O"
+                }
+            }
+            //No winner
+            //else if ((board[0] == "X" || board[0] == "O") &&
+            //       (board[0] == "X" || board[0] == "O")
+            //  )
+            //int i = Array.IndexOf(board, "X");
+
+            //else if (xyz.Contains("X"))
+            //{
+            //     Console.WriteLine("No Winner");//"C"
+            //}
+            else if (
+                placeHolder.Contains("1") ||
+                placeHolder.Contains("2") ||
+                placeHolder.Contains("3") ||
+                placeHolder.Contains("4") ||
+                placeHolder.Contains("5") ||
+                placeHolder.Contains("6") ||
+                placeHolder.Contains("7") ||
+                placeHolder.Contains("8") ||
+                placeHolder.Contains("9"))
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("C");
+            }
         }
     }
 }
+
